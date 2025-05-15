b28cd7374c3fbbd5b8bb7db93ba106ab04f0833e
Tyler Sheehan <tjsheeha@uvm.edu>
2025-05-15 12:16:42 -0400

    added basic html and flask

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify, make_response
+import json
+import sqlite3
+from sqlite3 import IntegrityError
+
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("homepage.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
